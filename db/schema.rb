f40dd1915f0db377387c49dadf02546e7b613c31
Rebecca Mills <vena.cava@gmail.com>
2015-02-26 13:00:28 -0500

    switching to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20150224191430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "buses", force: :cascade do |t|
     t.integer  "issue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "buses", ["issue_id"], name: "index_buses_on_issue_id"
+  add_index "buses", ["issue_id"], name: "index_buses_on_issue_id", using: :btree
 
   create_table "favorites", force: :cascade do |t|
     t.string   "busroute"
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 20150224191430) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "issues", force: :cascade do |t|
     t.string   "title"
@@ -58,7 +61,8 @@ ActiveRecord::Schema.define(version: 20150224191430) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "buses", "issues"
 end
